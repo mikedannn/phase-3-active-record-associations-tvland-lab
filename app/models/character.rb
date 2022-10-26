9e4e479e0fc726d678dc02e183a408372e496cf4
Michael <michael.daniels1013@gmail.com>
2022-10-26 13:28:43 -0500

    commit tree db3199207d94ebde772a12f15c868dd025044423

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,9 @@
 class Character < ActiveRecord::Base
-  
+    #join table
+    belongs_to :show
+    belongs_to :actor
+
+    def say_that_thing_you_say 
+        "#{self.name} always says: #{self.catchphrase}"
+    end
 end
